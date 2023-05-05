ccaa2f00e5261a42baf7f250bbb009b5fcf072ec
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-05-06 00:06:23 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -5,29 +5,16 @@ $('.rips_slide').slick({
     speed: 300,
     slidesToShow: 2,
     slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+  });
+
+  // approachSlider slaider----------
+$('.approachSlider').slick({
+    dots: false,
+    arrows:false,
+    infinite: true,
+    autoplay:true,
+    autoplaySpeed:2000,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   });
